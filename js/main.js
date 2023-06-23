f1baf981e74e2859ae0e0db51e1f5d218bd2f8e9
nakata0047 <n.craft0047r@gmail.com>
2023-06-23 09:17:07 +0900

    取り消し線ボタン実装

--- js/main.js
+++ js/main.js
@@ -3,30 +3,44 @@
 const textInput = document.getElementById('text-input');
 const todoList = document.getElementById('todo-list');
 
-textInput.addEventListener('keydown',e => {
+textInput.addEventListener('keydown', e => {
     const text = textInput.value;
 
-    if(text === '' || e.key !== 'Enter'){
+    if (text === '' || e.key !== 'Enter') {
         return;
     }
 
     const li = document.createElement('li');
     const span = document.createElement('span');
-    const button = document.createElement('button');
+    const delete_button = document.createElement('button');
+    const complete_button = document.createElement('button');
 
     li.classList.add('list-item');
     span.textContent = text;
     span.classList.add('todo-text');
+    span.id = "todo";
 
-    button.textContent = '削除';
-    button.type = 'button';
-    button.classList.add('delete-button');
-    button.addEventListener('click',e =>{
+    complete_button.textContent = '完了';
+    complete_button.type = 'button';
+    complete_button.classList.add('complete-button');
+    complete_button.addEventListener('click', e => {
+        if (todo.style.textDecoration == "line-through") {
+            todo.style.textDecoration = "none";
+        } else {
+            todo.style.textDecoration = "line-through";
+        }
+    });
+
+    delete_button.textContent = '削除';
+    delete_button.type = 'button';
+    delete_button.classList.add('delete-button');
+    delete_button.addEventListener('click', e => {
         todoList.removeChild(e.target.closest('li'));
     });
 
     li.appendChild(span);
-    li.appendChild(button);
+    li.appendChild(complete_button);
+    li.appendChild(delete_button);
     todoList.appendChild(li);
 
     textInput.value = '';
